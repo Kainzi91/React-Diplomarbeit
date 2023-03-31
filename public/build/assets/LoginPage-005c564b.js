8bf0c7d35fc313a56633b12aa91401b80e7b0aba
Alexander Kainz <kainza2@yahoo.de>
2023-03-31 15:30:14 +0200

    letzer Versuch

--- public/build/assets/LoginPage-005c564b.js
+++
@@ -1,1 +0,0 @@
-import{a as r,F as t,j as o,n as i}from"./app-fa0c9f22.js";import n from"./Login-882977b7.js";import"./GuestLayout-4efea962.js";import"./TextInput-0425f5d3.js";import"./InputLabel-58aaac9e.js";import"./PrimaryButton-4763cb58.js";function j(a){return r(t,{children:[o(i,{title:"Login"}),o(n,{})]})}export{j as default};
